a9425989a77de6c87213d72e763cd8ad87f7e686
Paula <128493887+PaulaMaged@users.noreply.github.com>
2024-04-14 20:31:12 +0200

    test schedparam struct of pthread attributes

--- Schedulers.c
+++ Schedulers.c
@@ -7,6 +7,7 @@ int main() {
     pthread_t t1;
     int policy;
     int schedpolicyInherit;
+    struct sched_param priority;
     pthread_attr_init(&tattr);
 
     pthread_attr_setschedpolicy(&tattr, SCHED_FIFO);
@@ -24,4 +25,9 @@ int main() {
     pthread_attr_getinheritsched(&tattr, &schedpolicyInherit);
     printf("The inheritedSchedPolicy is: %d\n", schedpolicyInherit);
     printf("The schedpolicy value is: %d\n", policy);
+
+    //get schedparam
+    pthread_attr_getschedparam(&tattr, &priority);
+
+    printf("The scheduling priority of thread is: %d\n", priority.sched_priority);
 }
--- ThreadsInC.c
+++ ThreadsInC.c
@@ -4,31 +4,40 @@
 
 void * ThreadFunction(void *arguments)
 {
-  unsigned long tid = pthread_self();
+  pthread_t tid = pthread_self();
+  int policy;
+  struct sched_param priority;
+
   for(int i = 1; i <= 5; i++) {
-    printf("%lu Thread Running for the %d/5\n", tid, i);
+    pthread_getschedparam(tid, &policy, &priority);
+    printf("%lu Thread Running for the %d/5\tThe thread's priority is: %d\n", tid, i, priority.sched_priority);
   }
   return NULL;
 }
 
 int main() {
-  
+
 // variables that will hold the ID of the threads created
   pthread_t thread1;
   pthread_t thread2;
   pthread_t thread3;
   pthread_t thread4;
-  
+
+  //using attr features of pthread
+  pthread_attr_t tattr;
+  pthread_attr_init(&tattr);
+  pthread_attr_setschedpolicy(&tattr, SCHED_FIFO);
+  // pthread_attr_setinheritsched(&tattr, PTHREAD_EXPLICIT_SCHED);
   printf("before creating threads\n");
 
   // Creating the threads 
-  pthread_create(&thread1, NULL, ThreadFunction, NULL);
+  pthread_create(&thread1, &tattr, ThreadFunction, NULL);
     printf("Newly created thread: %lu\n", thread1);
-  pthread_create(&thread2, NULL, ThreadFunction, NULL);
+  pthread_create(&thread2, &tattr, ThreadFunction, NULL);
     printf("Newly created thread: %lu\n", thread2);
-  pthread_create(&thread3, NULL, ThreadFunction, NULL);
+  pthread_create(&thread3, &tattr, ThreadFunction, NULL);
     printf("Newly created thread: %lu\n", thread3);
-  pthread_create(&thread4, NULL, ThreadFunction, NULL);
+  pthread_create(&thread4, &tattr, ThreadFunction, NULL);
     printf("Newly created thread: %lu\n", thread4);
 
 // waiting for threads to finish their functions before ending Main thread
