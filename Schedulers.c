2919c548fbf7488047f2520c6c54c2325ee95e2a
Paula <128493887+PaulaMaged@users.noreply.github.com>
2024-04-14 23:03:42 +0200

    testing schedinherit & consequences

--- Schedulers.c
+++ Schedulers.c
@@ -1,5 +1,13 @@
 #include <pthread.h>
 #include<stdio.h>
+#include <errno.h>
+
+int t1Set = -1;
+
+void* threadMethod(void* arg) {
+    t1Set = 1;
+    return NULL;
+}
 
 int main() {
     
@@ -19,7 +27,7 @@ int main() {
     printf("The inheritedSchedPolicy is: %d\n", schedpolicyInherit);
     printf("The schedpolicy value is: %d\n", policy);
 
-    pthread_attr_setinheritsched(&tattr, PTHREAD_EXPLICIT_SCHED);
+    pthread_attr_setinheritsched(&tattr, PTHREAD_INHERIT_SCHED);
 
     //After setting inheritSchedPolicy 
     pthread_attr_getinheritsched(&tattr, &schedpolicyInherit);
@@ -30,4 +38,27 @@ int main() {
     pthread_attr_getschedparam(&tattr, &priority);
 
     printf("The scheduling priority of thread is: %d\n", priority.sched_priority);
+
+    //Change main thread scheduling priority
+    pthread_t mainThread = pthread_self();
+    int mainPolicy;
+    struct sched_param mainPriority;
+    pthread_getschedparam(mainThread, &mainPolicy, &mainPriority);
+    printf("The mainThread has\n" 
+            "ID      : %lu\n"
+            "policy  : %d\n"
+            "priority: %d\n",
+             mainThread, mainPolicy, mainPriority.sched_priority);
+
+    //checking for privileges of setting a thread's scheduling policy
+    int ret = pthread_create(&t1, &tattr, threadMethod, NULL);
+    fprintf(stderr, "t1 creation return value: %d\n", ret);
+    ret = pthread_join(t1, NULL);
+    printf("t1 join return value: %d\n", ret);
+
+    printf("t1Set is: %d\n", t1Set);
+
+//EINVAL Invalid settings in attr. 22
+//EPERM  No permission to set the scheduling policy and parameters specified in attr. 1
+//EAGAIN Insufficient resources to create another thread. 11
 }
--- ThreadsInC.c
+++ ThreadsInC.c
@@ -1,6 +1,7 @@
 // Importing the Pthread Library
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void * ThreadFunction(void *arguments)
 {
@@ -10,13 +11,14 @@ void * ThreadFunction(void *arguments)
 
   for(int i = 1; i <= 5; i++) {
     pthread_getschedparam(tid, &policy, &priority);
-    printf("%lu Thread Running for the %d/5\tThe thread's priority is: %d\n", tid, i, priority.sched_priority);
+    printf("%lu Thread Running for the %d/5\tPriority is: %d\tPolicy is: %d\n", tid, i, priority.sched_priority, policy);
   }
   return NULL;
 }
 
 int main() {
 
+  int ret;
 // variables that will hold the ID of the threads created
   pthread_t thread1;
   pthread_t thread2;
@@ -25,27 +27,79 @@ int main() {
 
   //using attr features of pthread
   pthread_attr_t tattr;
-  pthread_attr_init(&tattr);
-  pthread_attr_setschedpolicy(&tattr, SCHED_FIFO);
-  // pthread_attr_setinheritsched(&tattr, PTHREAD_EXPLICIT_SCHED);
+  struct sched_param schedParam;
+  schedParam.sched_priority = 0;
+
+  ret = pthread_attr_init(&tattr);
+  if(ret != 0) {
+    printf("pthread Initialization: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
+  ret = pthread_attr_setschedpolicy(&tattr, SCHED_OTHER);
+  if(ret != 0) {
+    printf("pthread schedule policy: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
+  ret = pthread_attr_setinheritsched(&tattr, PTHREAD_INHERIT_SCHED);
+  if(ret != 0) {
+    printf("pthread inherit scheduling: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
+  ret = pthread_attr_setschedparam(&tattr, &schedParam);
+  if(ret != 0) {
+    printf("Setting scheduling priority failure: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
+
   printf("before creating threads\n");
 
   // Creating the threads 
-  pthread_create(&thread1, &tattr, ThreadFunction, NULL);
+  ret = pthread_create(&thread1, &tattr, ThreadFunction, NULL);
+  if(ret != 0) {
+    printf("pthread1 creation: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
     printf("Newly created thread: %lu\n", thread1);
-  pthread_create(&thread2, &tattr, ThreadFunction, NULL);
+  ret = pthread_create(&thread2, &tattr, ThreadFunction, NULL);
+  if(ret != 0) {
+    printf("pthread2 creation: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
     printf("Newly created thread: %lu\n", thread2);
-  pthread_create(&thread3, &tattr, ThreadFunction, NULL);
+  ret = pthread_create(&thread3, &tattr, ThreadFunction, NULL);
+  if(ret != 0) {
+    printf("pthread3 creation: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
     printf("Newly created thread: %lu\n", thread3);
-  pthread_create(&thread4, &tattr, ThreadFunction, NULL);
+  ret = pthread_create(&thread4, &tattr, ThreadFunction, NULL);
+  if(ret != 0) {
+    printf("pthread4 creation: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
     printf("Newly created thread: %lu\n", thread4);
 
 // waiting for threads to finish their functions before ending Main thread
-  pthread_join(thread1, NULL);
-  pthread_join(thread2, NULL);
-  pthread_join(thread3, NULL);
-  pthread_join(thread4, NULL);
+  ret = pthread_join(thread1, NULL);
+  if(ret != 0){
+    printf("join thread 1 failed: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
+  ret = pthread_join(thread2, NULL);
+  if(ret != 0){
+    printf("join thread 2 failed: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
+  ret = pthread_join(thread3, NULL);
+  if(ret != 0){
+    printf("join thread 3 failed: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
+  ret = pthread_join(thread4, NULL);
+  if(ret != 0){
+    printf("join thread 4 failed: %d\n", ret);
+    exit(EXIT_FAILURE);
+  }
 
-
-  return 0;
+  return EXIT_SUCCESS;
 }
