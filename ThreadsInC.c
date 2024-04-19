9c52037d90679f0b72ac39d45514f3eb13ff68af
Paula <128493887+PaulaMaged@users.noreply.github.com>
2024-04-19 14:20:37 +0200

    add burst time to threads

--- ThreadsInC.c
+++ ThreadsInC.c
@@ -5,16 +5,31 @@
 #include <string.h>
 #include <errno.h>
 #include <sched.h>
+#include <time.h>
+#include <unistd.h>
+
+int dummy = 0;
+struct timespec thread1Time;
+struct timespec thread2Time;
+struct timespec thread3Time;
+struct timespec thread4Time;
+
+void display(struct timespec timeSpec, int num_id) {
+    printf("Thread%d took %d.%.9ld\n", num_id, (int)timeSpec.tv_sec, timeSpec.tv_nsec);
+}
 
 void * ThreadFunction(void *arguments)
 {
   pthread_t tid = pthread_self();
 
+  clock_gettime(CLOCK_THREAD_CPUTIME_ID, arguments);
   for(int i = 1; i <= 5; i++) {
-    printf("%lu Thread Running for the %d/5\n", tid, i);
+    // printf("%lu Thread Running for the %d/5\n", tid, i);
+    dummy++;
   }
   return NULL;
 }
+
 
 int main() {
 
@@ -29,7 +44,7 @@ int main() {
   pthread_attr_t tattr;
   struct sched_param schedParam = 
   {
-    .sched_priority = 99,
+    .sched_priority = 0,
   };
 
   ret = pthread_attr_init(&tattr);
@@ -37,7 +52,7 @@ int main() {
     printf("pthread Initialization: %s\n", strerror(ret));
     exit(EXIT_FAILURE);
   }
-  ret = pthread_attr_setschedpolicy(&tattr, SCHED_FIFO);
+  ret = pthread_attr_setschedpolicy(&tattr, SCHED_OTHER);
   if(ret != 0) {
     printf("pthread schedule policy: %s\n", strerror(ret));
     exit(EXIT_FAILURE);
@@ -53,28 +68,28 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
-  printf("before creating threads\n");
+  // printf("before creating threads\n");
 
   // Creating the threads 
-  ret = pthread_create(&thread1, &tattr, ThreadFunction, NULL);
+  ret = pthread_create(&thread1, &tattr, ThreadFunction, (void *) &thread1Time);
   if(ret != 0) {
     printf("pthread1 creation: %s\n", strerror(ret));
     exit(EXIT_FAILURE);
   }
 
-  ret = pthread_create(&thread2, &tattr, ThreadFunction, NULL);
+  ret = pthread_create(&thread2, &tattr, ThreadFunction, (void *) &thread2Time);
   if(ret != 0) {
     printf("pthread2 creation: %s\n", strerror(ret));
     exit(EXIT_FAILURE);
   }
 
-  ret = pthread_create(&thread3, &tattr, ThreadFunction, NULL);
+  ret = pthread_create(&thread3, &tattr, ThreadFunction, (void *) &thread3Time);
   if(ret != 0) {
     printf("pthread3 creation: %s\n", strerror(ret));
     exit(EXIT_FAILURE);
   }
 
-  ret = pthread_create(&thread4, &tattr, ThreadFunction, NULL);
+  ret = pthread_create(&thread4, &tattr, ThreadFunction, (void *) &thread4Time);
   if(ret != 0) {
     printf("pthread4 creation: %s\n", strerror(ret));
     exit(EXIT_FAILURE);
@@ -101,6 +116,11 @@ int main() {
     printf("join thread 4 failed: %s\n", strerror(ret));
     exit(EXIT_FAILURE);
   }
+
+  display(thread1Time, 1);
+  display(thread2Time, 2);
+  display(thread3Time, 3);
+  display(thread4Time, 4);
 
   return EXIT_SUCCESS;
 }
--- performance.c
+++ performance.c
@@ -13,11 +13,6 @@ void sub_timespec(struct timespec t1, struct timespec t2, struct timespec *td)
         td->tv_nsec += NS_PER_SECOND;
         td->tv_sec--;
     }
-    else if (td->tv_sec < 0 && td->tv_nsec > 0)
-    {
-        td->tv_nsec -= NS_PER_SECOND;
-        td->tv_sec++;
-    }
 }
 
 int main(void)
@@ -25,11 +20,11 @@ int main(void)
     int dummy = 0;
     struct timespec start, finish, delta;
     clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start);
-    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &finish);
     for(int i = 0; i < 1000000000; i++) {
         dummy++;
     }
-    sleep(5);
+    // sleep(5);
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &finish);
     sub_timespec(start, finish, &delta);
     printf("%d.%.9ld\n", (int)delta.tv_sec, delta.tv_nsec);
     return 0;
