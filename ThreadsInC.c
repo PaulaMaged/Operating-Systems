e044cfc25fac22cf60cd2c19b81ba50961e6d258
Paula <128493887+PaulaMaged@users.noreply.github.com>
2024-04-19 17:57:46 +0200

    not working

--- ThreadsInC.c
+++ ThreadsInC.c
@@ -8,119 +8,140 @@
 #include <time.h>
 #include <unistd.h>
 
-int dummy = 0;
-struct timespec thread1Time;
-struct timespec thread2Time;
-struct timespec thread3Time;
-struct timespec thread4Time;
-
-void display(struct timespec timeSpec, int num_id) {
-    printf("Thread%d took %d.%.9ld\n", num_id, (int)timeSpec.tv_sec, timeSpec.tv_nsec);
-}
+struct threadMetrics {
+    pthread_t tid;
+    struct timespec burstTime;
+    double tat;
+};
 
 void * ThreadFunction(void *arguments)
 {
-  pthread_t tid = pthread_self();
+    int dummy = 0;
+    struct timespec burst;
+    double timeStart = (double) clock();
+    for(int i = 1; i <= 100; i++) {
+        dummy++;
+    }
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &burst); //for calculating burst time
+    double timeEnd = (double) clock(); //for calculating tat time
 
-  clock_gettime(CLOCK_THREAD_CPUTIME_ID, arguments);
-  for(int i = 1; i <= 5; i++) {
-    // printf("%lu Thread Running for the %d/5\n", tid, i);
-    dummy++;
-  }
-  return NULL;
+    //calculations
+    double timeDiff = timeEnd - timeStart; 
+    double timeTaken = timeDiff / CLOCKS_PER_SEC;
+
+    //return results
+    struct threadMetrics currentThread = *((struct threadMetrics*) arguments);
+    currentThread.burstTime = burst;
+    currentThread.tat = timeTaken;
+
+    return NULL;
+}
+void createTattr(pthread_attr_t *tattr, int policy, int inherit, struct sched_param *schedParam) {
+    
+    int ret = 0;
+    ret = pthread_attr_init(tattr);
+    if(ret != 0) {
+        printf("pthread Initialization: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+    ret = pthread_attr_setschedpolicy(tattr, policy);
+    if(ret != 0) {
+        printf("pthread schedule policy: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+    ret = pthread_attr_setinheritsched(tattr, inherit);
+    if(ret != 0) {
+        printf("pthread inherit scheduling: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    } 
+    ret = pthread_attr_setschedparam(tattr, schedParam);
+    if(ret != 0) {
+        printf("Setting scheduling priority failure: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
 }
 
+void displayThreadMetrics(struct threadMetrics *tM) {
+    struct threadMetrics thread = *tM;
+    printf("%lu\t", thread.tid);
+    printf("%d.%.9ld\t", (int)thread.burstTime.tv_sec, thread.burstTime.tv_nsec);
+    printf("%lf\n\n", thread.tat);
+}
 
 int main() {
+    //setup column headers
+    printf("Thread ID\tburst time\tTurn around time\n\n");
 
-  int ret;
-// variables that will hold the ID of the threads created
-  pthread_t thread1;
-  pthread_t thread2;
-  pthread_t thread3;
-  pthread_t thread4;
+    //setting attr features of pthread
+    struct sched_param schedParam = 
+        {
+            .sched_priority = 1,
+        };
+    pthread_attr_t tattr;
+    createTattr(&tattr, SCHED_FIFO, PTHREAD_EXPLICIT_SCHED, &schedParam);
 
-  //using attr features of pthread
-  pthread_attr_t tattr;
-  struct sched_param schedParam = 
-  {
-    .sched_priority = 0,
-  };
+    int ret;
+    // Creating the threads
+    struct threadMetrics thread1;
+    pthread_t tid1;
+    thread1.tid = tid1;
+    ret = pthread_create(&tid1, &tattr, ThreadFunction, (void *) &thread1);
+    if(ret != 0) {
+        printf("pthread1 creation: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+    struct threadMetrics thread2;
+    pthread_t tid2;
+    thread2.tid = tid2;
+    ret = pthread_create(&tid2, &tattr, ThreadFunction, (void *) &thread2);
+    if(ret != 0) {
+        printf("pthread2 creation: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+    struct threadMetrics thread3;
+    pthread_t tid3;
+    thread3.tid = tid3;
+    ret = pthread_create(&tid3, &tattr, ThreadFunction, (void *) &thread3);
+    if(ret != 0) {
+        printf("pthread3 creation: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+    struct threadMetrics thread4;
+    pthread_t tid4;
+    thread4.tid = tid4;
+    ret = pthread_create(&tid4, &tattr, ThreadFunction, (void *) &thread4);
+    if(ret != 0) {
+        printf("pthread4 creation: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
 
-  ret = pthread_attr_init(&tattr);
-  if(ret != 0) {
-    printf("pthread Initialization: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-  ret = pthread_attr_setschedpolicy(&tattr, SCHED_OTHER);
-  if(ret != 0) {
-    printf("pthread schedule policy: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-  ret = pthread_attr_setinheritsched(&tattr, PTHREAD_EXPLICIT_SCHED);
-  if(ret != 0) {
-    printf("pthread inherit scheduling: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  } 
-  ret = pthread_attr_setschedparam(&tattr, &schedParam);
-  if(ret != 0) {
-    printf("Setting scheduling priority failure: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
+    // waiting for threads to finish their functions before ending Main thread
+    ret = pthread_join(tid1, NULL);
+    if(ret != 0){
+        printf("join thread 1 failed: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+    ret = pthread_join(tid2, NULL);
+    if(ret != 0){
+        printf("join thread 2 failed: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+    ret = pthread_join(tid3, NULL);
+    if(ret != 0){
+        printf("join thread 3 failed: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
+    ret = pthread_join(tid4, NULL);
+    if(ret != 0){
+        printf("join thread 4 failed: %s\n", strerror(ret));
+        exit(EXIT_FAILURE);
+    }
 
-  // printf("before creating threads\n");
+    //results
+    displayThreadMetrics(&thread1);
+    displayThreadMetrics(&thread2);
+    displayThreadMetrics(&thread3);
+    displayThreadMetrics(&thread4);
 
-  // Creating the threads 
-  ret = pthread_create(&thread1, &tattr, ThreadFunction, (void *) &thread1Time);
-  if(ret != 0) {
-    printf("pthread1 creation: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-
-  ret = pthread_create(&thread2, &tattr, ThreadFunction, (void *) &thread2Time);
-  if(ret != 0) {
-    printf("pthread2 creation: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-
-  ret = pthread_create(&thread3, &tattr, ThreadFunction, (void *) &thread3Time);
-  if(ret != 0) {
-    printf("pthread3 creation: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-
-  ret = pthread_create(&thread4, &tattr, ThreadFunction, (void *) &thread4Time);
-  if(ret != 0) {
-    printf("pthread4 creation: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-
-// waiting for threads to finish their functions before ending Main thread
-  ret = pthread_join(thread1, NULL);
-  if(ret != 0){
-    printf("join thread 1 failed: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-  ret = pthread_join(thread2, NULL);
-  if(ret != 0){
-    printf("join thread 2 failed: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-  ret = pthread_join(thread3, NULL);
-  if(ret != 0){
-    printf("join thread 3 failed: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-  ret = pthread_join(thread4, NULL);
-  if(ret != 0){
-    printf("join thread 4 failed: %s\n", strerror(ret));
-    exit(EXIT_FAILURE);
-  }
-
-  display(thread1Time, 1);
-  display(thread2Time, 2);
-  display(thread3Time, 3);
-  display(thread4Time, 4);
-
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
